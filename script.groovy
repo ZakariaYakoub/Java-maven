f3e416158c5a400f024464bb9eb4a961eb62f771
Zakaria Yakoub <zakaria.ait.yakoub@gmail.com>
2023-02-10 15:45:47 +0100

    added groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def BuildApp() {
+    echo "Building the application..."
+}
+
+def TestApp() {
+    echo "Testing the application..."
+}
+
+def DeployApp() {
+    echo "Deploying the application..."
+}
+
+return this
